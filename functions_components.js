171d40768e3477c55b6d0fd5d0a3f4cab7bf6290
sergiogarcez <seegio.jr.10@gmail.com>
2025-04-20 19:42:33 -0400

    "Feat: Changing swiper and footer"

--- functions_components.js
+++ functions_components.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         clickable: true
         },
         autoplay: { // Auto play dos slides
-        delay: 3000
+        delay: 4000
         }
     });
 });
